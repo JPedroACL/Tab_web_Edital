3588e86df6d77550dbc7ac038d0fd0b0142646ce
Lucas de Arimateia Coutinho da Silva <93049848+LuscasArimate@users.noreply.github.com>
2023-05-07 18:38:56 -0300

    Iniciado a responsividade

--- form_web/js/MaskInput.js
+++ form_web/js/MaskInput.js
@@ -63,3 +63,22 @@ function mascaraTel(mascaraInput) {
   }
   
   
+
+  
+
+function mais(){
+  var atual = document.getElementById("total").value;
+  var novo = atual - (-1); //Evitando Concatenacoes
+  document.getElementById("total").value = novo;
+}
+
+function menos(){
+  var atual = document.getElementById("total").value;
+  if(atual > 0) { //evita números negativos
+    var novo = atual - 1;
+    document.getElementById("total").value = novo;
+  }
+}
+
+  
+  
